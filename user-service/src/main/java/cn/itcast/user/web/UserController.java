708fe0148f084877204333163f2903b8f2263d2a
zhouhw0306 <2466864587@qq.com>
2022-11-18 20:47:25 +0800

    2022/11/18

---
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -0,0 +1,53 @@
+package cn.itcast.user.web;
+
+import cn.itcast.user.config.PatternProperties;
+import cn.itcast.user.pojo.User;
+import cn.itcast.user.service.UserService;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+//@RefreshScope //配置文件热更新
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Value("${pattern.dateformat}")
+    private String dateformat;
+
+    @Autowired
+    private PatternProperties properties;
+
+    @GetMapping("prop")
+    public PatternProperties properties(){
+        return properties;
+    }
+
+    @GetMapping("now")
+    public String now(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(properties.getDateformat()));
+    }
+
+    /**
+     * 路径： /user/110
+     *
+     * @param id 用户id
+     * @return 用户
+     */
+    @GetMapping("/{id}")
+    public User queryById(@PathVariable("id") Long id,
+                          @RequestHeader(value = "Truth", required = false) String truth) {
+        System.out.println("truth: " + truth);
+        return userService.queryById(id);
+    }
+}
